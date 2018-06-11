af1f10180f025a5b8e6f07329759502037d64c2a
asconsulting <andrew@andrewstevens.consulting>
2018-06-11 16:38:41 -0400

    Debugging

--- system/modules/zyppy_class/library/Asc/Frontend/ZyppyClass.php
+++ system/modules/zyppy_class/library/Asc/Frontend/ZyppyClass.php
@@ -15,13 +15,13 @@ namespace Asc\Frontend;
 class ZyppyClass extends \Frontend
 {
 	public function compileArticle($objTemplate, $arrData, $objModule) {
-		var_dump($objTemplate);
-		echo "<br><br>";
+		//var_dump($objTemplate);
+		//echo "<br><br>";
 		
-		var_dump($arrData);
-		echo "<br><br>";
+	//	var_dump($arrData);
+	//	echo "<br><br>";
 		
-		var_dump($objModule);
-		echo "<br><br>";
+	//	var_dump($objModule);
+	//	echo "<br><br>";
 	}
 }
