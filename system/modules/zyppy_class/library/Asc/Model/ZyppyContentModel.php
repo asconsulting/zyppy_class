bc0b445e08288a4033ba0efcef30e480e003a7e0
asconsulting <andrew@andrewstevens.consulting>
2018-06-12 16:51:13 -0400

    Content

--- system/modules/zyppy_class/library/Asc/Model/ZyppyContentModel.php
+++ system/modules/zyppy_class/library/Asc/Model/ZyppyContentModel.php
@@ -12,6 +12,7 @@
 namespace Asc\Model;
 
 use Contao\Database;
+use Contao\FormModel;
 use Contao\ModuleModel;
 
 class ZyppyContentModel extends \Contao\ContentModel
@@ -21,26 +22,37 @@ class ZyppyContentModel extends \Contao\ContentModel
 	{
 		parent::__construct($objResult);
 		
+		$arrCss = \StringUtil::deserialize($this->cssID, true);
 		if ($this->type == 'module') {
 			$objModule = ModuleModel::findByPk($this->module);
 			if ($objModule) {
-				$arrCss = \StringUtil::deserialize($objModule->cssID, true);
-				$arrCss[1] .= ' zyppy_module ' .$objModule->cssChooser;
+				$arrModuleCss = \StringUtil::deserialize($objModule->cssID, true);
+				$arrModuleCss[1] .= ' zyppy_module ' .$objModule->cssChooser;
 
 				$arrCommon = \StringUtil::deserialize($objModule->commonClasses, true);
 				if (!empty($arrCommon)) {
-					$arrCss[1] .= ' ' .implode(' ', $arrCommon);
+					$arrModuleCss[1] .= ' ' .implode(' ', $arrCommon);
 				}
-				$arrCss[1] = str_replace('  ', ' ', $arrCss[1]);
-				$arrCss[1] = trim($arrCss[1]);
+				$arrModuleCss[1] = str_replace('  ', ' ', $arrModuleCss[1]);
+				$arrCss[1] .= trim($arrModuleCss[1]);
 			}
-			var_dump($this);
-			echo "<br><hr><br><br>";
-			var_dump($objModule);
-			echo "<br><hr><br><br>";
+		}
+
+		if ($this->type == 'form') {
+			$objForm = FormModel::findByPk($this->form);
+			if ($objForm) {
+				$arrFormCss = \StringUtil::deserialize($objForm->cssID, true);
+				$arrFormCss[1] .= ' zyppy_module ' .$objForm->cssChooser;
+
+				$arrCommon = \StringUtil::deserialize($objForm->commonClasses, true);
+				if (!empty($arrCommon)) {
+					$arrFormCss[1] .= ' ' .implode(' ', $arrCommon);
+				}
+				$arrFormCss[1] = str_replace('  ', ' ', $arrFormCss[1]);
+				$arrCss[1] .= trim($arrFormCss[1]);
+			}
 		}
 		
-		$arrCss = \StringUtil::deserialize($this->cssID, true);
 		$arrCss[1] .= ' zyppy zyppy_content ' .$this->cssChooser;
 
 		$arrCommon = \StringUtil::deserialize($this->commonClasses, true);
