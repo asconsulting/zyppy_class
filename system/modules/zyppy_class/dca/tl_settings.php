b43c7d7488aa010a4bfa62c4bb6a59758baaf0c4
asconsulting <andrew@andrewstevens.consulting>
2018-06-13 02:49:53 -0400

    Test

--- system/modules/zyppy_class/dca/tl_settings.php
+++ system/modules/zyppy_class/dca/tl_settings.php
@@ -19,7 +19,7 @@ $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace(';{global
 $GLOBALS['TL_DCA']['tl_settings']['fields']['articleClasses'] = array(
 	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['articleClasses'],
 	'inputType'               => 'keyValueWizard',
-	'eval'                    => array('tl_class'=>'clr w50'),
+	'eval'                    => array('tl_class'=>'clr w50', 'style'=>'background-color:red;'),
 	'sql'                     => "meduimtext NOT NULL default ''"
 );
 $GLOBALS['TL_DCA']['tl_settings']['fields']['articleClassesRequired'] = array(
