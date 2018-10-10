c3779acbf4e41978a333e99e56d176eda4fe8dde
asconsulting <andrew@andrewstevens.consulting>
2018-10-10 12:28:14 -0400

    Namespace

--- system/modules/zyppy_class/library/Asc/Model/ZyppyPageModel.php
+++ system/modules/zyppy_class/library/Asc/Model/ZyppyPageModel.php
@@ -13,7 +13,7 @@ namespace Asc\Model;
 
 use Contao\Database;
 
-class ZyppyPageModel extends \Contao\PageModel
+class ZyppyPageModel extends \PageModel
 {
 
 	public function __construct(Database\Result $objResult=null)
