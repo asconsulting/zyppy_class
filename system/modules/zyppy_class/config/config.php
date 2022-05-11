6757f48e13a74e20f50f1a78089a14f5cd0df7fc
Andrew Stevens <andrew@andrewstevens.consulting>
2022-05-10 23:29:05 -0400

    Update config.php

--- system/modules/zyppy_class/config/config.php
+++ system/modules/zyppy_class/config/config.php
@@ -16,7 +16,7 @@
  */
 $GLOBALS['TL_HOOKS']['generatePage'][] 		= array('ZyppyClass\Frontend\ZyppyPage', 'generatePage');
 $GLOBALS['TL_HOOKS']['getArticle'][] 		= array('ZyppyClass\Frontend\ZyppyArticle', 'generateArticle');
-$GLOBALS['TL_HOOKS']['getContentElement'][] = array('ZyppyClass\Frontend\ZyppyContent', 'generateContent');
+//$GLOBALS['TL_HOOKS']['getContentElement'][] = array('ZyppyClass\Frontend\ZyppyContent', 'generateContent');
 //$GLOBALS['TL_HOOKS']['getForm'][] 			= array('ZyppyClass\Frontend\ZyppyContent', 'generateContent');
 //$GLOBALS['TL_HOOKS']['getFrontendModule'][] = array('ZyppyClass\Frontend\ZyppyContent', 'generateContent');
 //$GLOBALS['TL_HOOKS']['compileFormFields'][] = array('ZyppyClass\Frontend\ZyppyForm', 'compileFormFields');
