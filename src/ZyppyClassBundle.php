6f341c2f26a80040be1fef930c2b2be903892c7a
Andrew Stevens <andrew@andrewstevens.consulting>
2024-12-18 11:35:03 -0500

    Configure

--- src/ZyppyClassBundle.php
+++ src/ZyppyClassBundle.php
@@ -17,6 +17,16 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ZyppyClassBundle extends Bundle
 {
+	
+    public function loadExtension(
+        array $config, 
+        ContainerConfigurator $containerConfigurator, 
+        ContainerBuilder $containerBuilder,
+    ): void
+    {
+        $containerConfigurator->import('../config/services.yaml');
+    }
+	
     public function getPath(): string
     {
         return \dirname(__DIR__);
