ca89826d54b7a8d6d28e81669105fa56995140eb
Andrew Stevens <andrew@andrewstevens.consulting>
2024-12-18 12:53:48 -0500

    Class Options

--- src/EventListener/Backend/DataContainer/ClassOptionsListener.php
+++ src/EventListener/Backend/DataContainer/ClassOptionsListener.php
@@ -75,4 +75,84 @@ class ClassOptionsListener
 		}
 		return $arrOptions;
     }
+	
+	
+	#[AsCallback(table: 'tl_article', target: 'fields.globalCommonClasses.options')]
+	#[AsCallback(table: 'tl_content', target: 'fields.globalCommonClasses.options')]
+	#[AsCallback(table: 'tl_form', target: 'fields.globalCommonClasses.options')]
+	#[AsCallback(table: 'tl_form_field', target: 'fields.globalCommonClasses.options')]
+	#[AsCallback(table: 'tl_module', target: 'fields.globalCommonClasses.options')]
+	#[AsCallback(table: 'tl_page', target: 'fields.globalCommonClasses.options')]
+    public function getGlobalCommonOptions(DataContainer $dc): array
+    {
+		$arrOptions = array();
+
+		if (Config::get('globalCommonClasses') != '') {
+			$arrTemp = StringUtil::deserialize(Config::get('globalCommonClasses'), true);
+			foreach ($arrTemp as $arrOption) {
+				$arrOptions[$arrOption['key']] = $arrOption['value'];
+			}
+		}
+		return $arrOptions;
+    }
+	
+	
+	#[AsCallback(table: 'tl_article', target: 'fields.exclusiveClass.options')]
+	#[AsCallback(table: 'tl_content', target: 'fields.exclusiveClass.options')]
+	#[AsCallback(table: 'tl_form', target: 'fields.exclusiveClass.options')]
+	#[AsCallback(table: 'tl_form_field', target: 'fields.exclusiveClass.options')]
+	#[AsCallback(table: 'tl_module', target: 'fields.exclusiveClass.options')]
+	#[AsCallback(table: 'tl_page', target: 'fields.exclusiveClass.options')]
+    public function getExclusiveClassOptions(DataContainer $dc): array
+    {
+		$arrOptions = array();
+		$strClassRequired = false;
+		$strClasses = false;
+
+		switch($dc->table) {
+			case "tl_article":
+				$strClassRequired = 'articleClassesRequired';
+				$strClasses = 'articleClasses';
+			break;
+
+			case "tl_content":
+				$strClassRequired = 'contentClassesRequired';
+				$strClasses = 'contentClasses';
+			break;
+
+			case "tl_form_field":
+				$strClassRequired = 'formFieldClassesRequired';
+				$strClasses = 'formFieldClasses';
+			break;
+
+			case "tl_form":
+				$strClassRequired = 'formClassesRequired';
+				$strClasses = 'formClasses';
+			break;
+
+			case "tl_module":
+				$strClassRequired = 'moduleClassesRequired';
+				$strClasses = 'moduleClasses';
+			break;
+
+			case "tl_page":
+				$strClassRequired = 'pageClassesRequired';
+				$strClasses = 'pageClasses';
+			break;
+		}
+
+		$arrOptions = array();
+		if (!Config::get($strClassRequired)) {
+			$arrOptions[''] = '-';
+		}
+
+		if (Config::get($strClasses) != '') {
+			$arrTemp = StringUtil::deserialize(Config::get($strClasses), true);
+			foreach ($arrTemp as $arrOption) {
+				$arrOptions[$arrOption['key']] = $arrOption['value'];
+			}
+		}
+		return $arrOptions;
+    }
+	
 }
